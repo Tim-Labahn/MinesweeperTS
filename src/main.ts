6d64a35d1d7e83dad4040859c5bd8260dfdd5c34
tim-labahn <tim.labahn@freaknet.de>
2023-08-11 10:55:43 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css'
 let gameMap = [];
-const width = 37;
-const height = 15;
+let width = 23;
+let height = 11;
 //------------------------
 function game() {
+  checkSize();
   gameMap = [];
   generateField();
   generateBomb();
@@ -163,6 +164,21 @@ function checkWin() {
   }
 }
 
+function checkSize() {
+  /** @ts-expect-error @type HTMLSelectElement */
+  const gameMapSize = document.querySelector('select');
+  if (gameMapSize.value === 'small') {
+    width = 11;
+    height = 9;
+  } else if (gameMapSize.value === 'medium') {
+    width = 23;
+    height = 11;
+  } else {
+    width = 37;
+    height = 15;
+  }
+}
+
 function checkArea(x, y) {
   if (gameMap[x + 0]?.[y + 1]?.isOpen === false) {
     tileClick(x, y + 1);
@@ -190,4 +206,6 @@ function checkArea(x, y) {
   }
 }
 
-game();
+/** @ts-expect-error @type HTMLDialogElement */
+const options = document.querySelector('.start');
+options.showModal();
